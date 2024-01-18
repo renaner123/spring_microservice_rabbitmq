6c0c4fabd1fe55aa7b4bf556d3b20ee8388520b5
renaner123 <renaner123@gmail.com>
2024-01-18 14:24:44 -0300

    feat: add user producer

--- user/src/main/java/com/ms/user/services/UserService.java
+++ user/src/main/java/com/ms/user/services/UserService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ms.user.models.UserModel;
+import com.ms.user.producers.UserProducer;
 import com.ms.user.repositories.UserRepository;
 
 import jakarta.transaction.Transactional;
@@ -13,10 +14,15 @@ public class UserService {
 	
 	@Autowired
 	UserRepository userRepository;
+	
+	@Autowired
+	UserProducer userProducer;
 
 	@Transactional
 	public UserModel save(UserModel userModel) {
-		return userRepository.save(userModel);
+		userModel = userRepository.save(userModel);
+		userProducer.publishMessageEmail(userModel);
+		return userModel;
 	}
 
 }
