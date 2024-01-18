daa30f728721168aa6cf49ae2672d44e9ef74593
renaner123 <renaner123@gmail.com>
2024-01-18 14:22:06 -0300

    feat: configure emailSender

--- email/src/main/java/com/ms/email/consumers/EmailConsumer.java
+++ email/src/main/java/com/ms/email/consumers/EmailConsumer.java
@@ -1,16 +1,28 @@
 package com.ms.email.consumers;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 import com.ms.email.dtos.EmailRecordDto;
+import com.ms.email.models.EmailModel;
+import com.ms.email.services.EmailService;
 
 @Component
 public class EmailConsumer {
+	
+	final EmailService emailService;
+	
+	public EmailConsumer(EmailService emailService) {
+		this.emailService = emailService;
+	}
 
 	@RabbitListener(queues = "${broker.queue.email.name}")
 	public void listenerEmailQueue(@Payload EmailRecordDto emailrecordDto) {
-		System.out.println(emailrecordDto.emailTo());
+		var emailModel = new EmailModel();
+		BeanUtils.copyProperties(emailrecordDto, emailModel);
+		emailService.sendEmail(emailModel);
 	}
 }
